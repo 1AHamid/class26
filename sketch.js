c37dfb6667cff4e2ee5c39f7537f5e94fd03d919
lumine2412 <aryanhamid34@gmail.com>
2021-06-01 18:36:20 -0400

    plat

--- sketch.js
+++ sketch.js
@@ -5,6 +5,7 @@ const Bodies = Matter.Bodies;
 var engine, world;
 var box1, pig1;
 var backgroundImg;
+var platform;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -34,6 +35,7 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
+    platform = new Ground(150,305,300,170);
 
 }
 
@@ -43,6 +45,7 @@ function draw(){
     console.log(box2.body.position.x);
     console.log(box2.body.position.y);
     console.log(box2.body.angle);
+    platform.display();
     box1.display();
     box2.display();
     ground.display();
@@ -59,4 +62,5 @@ function draw(){
     log5.display();
 
     bird.display();
+    
 }
